f412befdec6353467fc46d513f0a00a9a81274ee
Gbsyi <gbsyi@yandex.ru>
2022-01-02 19:02:16 +0700

    .

---
+++ ApiApp/Controllers/UnauthorizedController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace ApiApp.Controllers
+{
+    [Route("api")]
+    public class UnauthorizedController : Controller
+    {
+        [Route("visibility")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(new { isServerVisible = true });
+        }
+    }
+}
